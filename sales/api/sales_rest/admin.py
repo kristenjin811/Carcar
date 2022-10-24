3d0db593b8f2ce387e50a657a92bcd353f7d8130
rachel <jrachelr1@gmail.com>
2022-10-24 16:23:55 -0700

    models and admin created

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Customer, SaleRecord, SalesPerson, AutomobileVO
 
 # Register your models here.
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SaleRecord)
+class SaleRecordAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SalesPerson)
+class SalesPersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,97 @@
+from email.policy import default
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class ManufacturerVO(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def get_api_url(self):
+        return reverse("api_manufacturer", kwargs={"pk": self.id})
+
+    def __str__(self):
+        return self.name
+
+
+class VehicleModelVO(models.Model):
+    name = models.CharField(max_length=100)
+    picture_url = models.URLField()
+
+    manufacturer = models.ForeignKey(
+        ManufacturerVO,
+        related_name="models",
+        on_delete=models.CASCADE,
+    )
+
+    def get_api_url(self):
+        return reverse("api_vehicle_model", kwargs={"pk": self.id})
+
+    def __str__(self):
+        return self.name
+
+
+class AutomobileVO(models.Model):
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+
+    model = models.ForeignKey(
+        VehicleModelVO,
+        related_name="automobiles",
+        on_delete=models.CASCADE,
+    )
+
+    available = models.BooleanField(default=True)
+
+    def get_api_url(self):
+        return reverse("api_automobile", kwargs={"vin": self.vin})
+
+    def __str__(self):
+        return f"{self.color} {self.model} VIN:{self.vin}"
+
+
+class SalesPerson(models.Model):
+    name = models.CharField(max_length=100)
+    employee_id = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.PositiveSmallIntegerField()
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class SaleRecord(models.Model):
+    car = models.ForeignKey(
+        AutomobileVO,
+        related_name="car",
+        null=False,
+        blank=False,
+        on_delete=models.PROTECT,
+    )
+    sales_person = models.ForeignKey(
+        SalesPerson,
+        related_name="sales_person",
+        null=False,
+        blank=False,
+        on_delete=models.PROTECT,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="customer",
+        null=False,
+        blank=False,
+        on_delete=models.PROTECT,
+    )
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.car} {self.sales_person} {self.price}"
