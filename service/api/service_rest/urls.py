8fff83012620a809e2d24670b81422e0de1cfaad
Kristen <kristenjinn@gmail.com>
2022-10-24 20:47:24 -0700

    appointments list request working in insomnia

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import api_list_service_appointments
+from .views import api_list_appointments
 
 urlpatterns = [
-  path("service_appointments/", api_list_service_appointments, name="api_create_service_appointments"),
+  path("service/", api_list_appointments, name="api_create_appointments"),
 ]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from .models import ServiceAppointment, Technician
+from common.json import ModelEncoder
+
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+class TechnicianListEncoder(ModelEncoder):
+  model = Technician
+  properties = ["name"]
+
+
+class AppointmentListEncoder(ModelEncoder):
+  model = ServiceAppointment
+  properties = ["VIN", "customer_name", "time", "technician", "reason"]
+  encoders = {
+    "technician": TechnicianListEncoder(),
+  }
+
+
+require_http_methods(["GET", "POST"])
+def api_list_appointments(request):
+  if request.method == "GET":
+    service_appointments = ServiceAppointment.objects.all()
+    return JsonResponse(
+      {"service_appointments": service_appointments},
+      encoder = AppointmentListEncoder,
+    )
