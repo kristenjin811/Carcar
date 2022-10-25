e8625985b134a7e1df0e93b78f5941d06feb3cc1
Kristen <kristenjinn@gmail.com>
2022-10-25 15:46:43 -0700

    working on the navbar

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AppointmentList from './AppointmentList';
+import AppointmentList from './service/AppointmentList';
 import MainPage from './MainPage';
 import Nav from './Nav';
 
