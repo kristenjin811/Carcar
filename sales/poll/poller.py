a82f90d3fe7df835cc1c4d5e82ae2006a71df11c
Rachel Johnson <jrachelr1@gmail.com>
2022-11-01 19:56:02 +0000

    Update ghi/app/src/Inventory/ModelForm.js, ghi/app/src/Inventory/ModelList.js, ghi/app/src/Sales/CustomerForm.js, ghi/app/src/Sales/SalesByRep.js, ghi/app/src/Sales/SalesList.js, ghi/app/src/Sales/SalesRecordForm.js, ghi/app/src/Sales/SalesRepForm.js, sales/poll/poller.py

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -31,7 +31,7 @@ def poll():
             get_autos()
         except Exception as e:
             print(e, file=sys.stderr)
-        time.sleep(5)
+        time.sleep(60)
 
 
 if __name__ == "__main__":
